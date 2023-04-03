f6351f7fbc38fc9af4417223797a3370d60be2b8
one-color-low <gakki.uec15@gmail.com>
2023-04-03 21:22:55 +0900

    ini

--- MizutamaCollage/ContentView.swift
+++ MizutamaCollage/ContentView.swift
@@ -6,17 +6,48 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    let videoCapture = VideoCapture()
+    @State var image: UIImage? = nil
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if let image = image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            }
+            HStack {
+                Button("run") {
+                    videoCapture.run { sampleBuffer in
+                        if let convertImage = UIImageFromSampleBuffer(sampleBuffer) {
+                            DispatchQueue.main.async {
+                                self.image = convertImage
+                            }
+                        }
+                    }
+                }
+                Button("stop") {
+                    videoCapture.stop()
+                }
+            }
+            .font(.largeTitle)
         }
-        .padding()
     }
+
+    func UIImageFromSampleBuffer(_ sampleBuffer: CMSampleBuffer) -> UIImage? {
+        if let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
+            let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
+            let imageRect = CGRect(x: 0, y: 0, width: CVPixelBufferGetWidth(pixelBuffer), height: CVPixelBufferGetHeight(pixelBuffer))
+            let context = CIContext()
+            if let image = context.createCGImage(ciImage, from: imageRect) {
+                return UIImage(cgImage: image)
+            }
+        }
+        return nil
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
