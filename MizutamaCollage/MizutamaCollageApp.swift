307567a7e5fdfd7ccd51c899f44510d977046544
one-color-low <gakki.uec15@gmail.com>
2023-04-03 20:07:41 +0900

    Initial Commit

---
+++ MizutamaCollage/MizutamaCollageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MizutamaCollageApp.swift
+//  MizutamaCollage
+//
+//  Created by Yuta on 2023/04/03.
+//
+
+import SwiftUI
+
+@main
+struct MizutamaCollageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
